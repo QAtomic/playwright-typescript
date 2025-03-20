c0119f693f731a80e259d07e1b55c24ad8978acf
QAtomic <drewsore@comcast.net>
2025-03-19 21:03:40 -0400

    API test updates

--- test/api/tests/getUsers.spec.ts
+++ test/api/tests/getUsers.spec.ts
@@ -4,10 +4,23 @@ test.describe.parallel("API : Get Users", () => {
     const baseURL = "https://reqres.in";
 
     test("Get User Two", async ({ request }) => {
-        const response = await request.get(`${baseURL}/api/users`);
+        const response = await request.get(`${baseURL}/api/users/2`);
 
         expect(response.status()).toBe(200);
+
+        const responseBody = JSON.parse(await response.text());
+
+        //console.log(responseBody);
+
+        expect(responseBody.data.id).toBe(2);
+        expect(responseBody.data.first_name).toBe("Janet");
+        expect(responseBody.data.last_name).toBe("Weaver");
+
+        expect(responseBody.support.url).toContain("contentcaddy");
+
+        //console.log(responseBody.support.url)
     });
+    
 
     test("User Not Found", async ({ request }) => {
         const response = await request.get(`${baseURL}/api/users/23`);
