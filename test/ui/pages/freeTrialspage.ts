cfbf2ba2d1271c6e266d9bb370c12cfa1cdc5ce3
QAtomic <drewsore@comcast.net>
2025-03-27 06:44:31 -0400

    Try Now Workflow addition

---
+++ test/ui/pages/freeTrialspage.ts
@@ -0,0 +1,39 @@
+import { Page, expect, Locator } from '@playwright/test';
+import { closeCookiePopup } from '../utils/closeCookiePopup';
+import { brokenLinkChecker } from '../utils/brokenLinkChecker';
+
+export class FreeTrialsPage {
+    page: Page;
+    url: string;
+
+
+    constructor(page: Page) {
+        this.page = page;
+        this.url = "https://www.telerik.com/download";
+    };
+
+
+    async open() {
+        await this.page.goto(this.url);
+        await closeCookiePopup(this.page);
+    }
+
+    async verifyPageTitle() {
+        let pricingPageTitle = await this.page.title();
+        expect(pricingPageTitle).toBe("Download trials and sign up for services | Telerik");
+    }
+
+    async checkForBrokenLinks() {
+        await brokenLinkChecker(this.page);
+    }
+
+    async clickTryNowLink() {
+        await this.page.getByText('Telerik DevCraftBuild').getByRole('link', { name: 'Try now' }).click();
+    }
+
+    async enterEmailInCreateAccountForm() {
+        await this.page.getByRole('textbox', { name: 'Work or Telerik Account Email' }).fill('playwright@test.com');
+    }
+
+
+};
---
+++ test/ui/tests/freeTrialsTests.spec.ts
@@ -0,0 +1,26 @@
+import { test } from '@playwright/test';
+import { FreeTrialsPage } from '../pages/freeTrialspage';
+import { sleep } from '../../utils/sleep';
+
+
+test.describe("Free Trials Tests", () => {
+    let freeTrialsPage: FreeTrialsPage;
+
+    test.beforeEach(async ({ page }) => {
+        freeTrialsPage = new FreeTrialsPage(page);
+        await freeTrialsPage.open();
+    }); 
+
+
+    test("Broken Link Checker", async () => {
+        test.setTimeout(120000);
+        await freeTrialsPage.checkForBrokenLinks();
+    });
+
+
+    test('Telerik DevCraft Try Now Workflow', async () => {
+        await freeTrialsPage.clickTryNowLink();
+        await freeTrialsPage.enterEmailInCreateAccountForm();
+        sleep(2000);
+    });
+});
